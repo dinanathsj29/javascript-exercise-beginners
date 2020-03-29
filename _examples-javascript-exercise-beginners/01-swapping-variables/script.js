bd8627d66604475373d08bf3f7b2d851c7fc8cf8
Dinanath Jayaswal <41334766+dinanathsj29@users.noreply.github.com>
2020-03-29 22:52:07 +0530

    example 1 - swapping variables uploaded
    example 1 - swapping variables uploaded

---
+++ _examples-javascript-exercise-beginners/01-swapping-variables/script.js
@@ -0,0 +1,16 @@
+console.log('01-swapping-variables');
+
+let value1 = 'one';
+let value2 = 'two';
+
+// original values
+console.log('original', value1);
+console.log('original', value2);
+
+// swapping values
+let value3 = value1;
+value1 = value2;
+value2 = value3;
+
+console.log('swap', value1);
+console.log('swap', value2);
